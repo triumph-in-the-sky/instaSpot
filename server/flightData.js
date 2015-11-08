f08c809a0beaf810079f9389832a71ee035b3a30
codeCommode <james.harlan@gmail.com>
2015-11-07 22:51:45 -0800

    [feat] fake times!

--- server/flightData.js
+++ server/flightData.js
@@ -3,78 +3,91 @@ module.exports = [
     origin:"San Francisco, USA",
     destination:"Budapest, Hungary", 
     departureDate:"11-12-2015",
+    time:"1200",
     fair:"1587USD"
   },
   {
     origin:"San Francisco, USA",
     destination:"Budapest, Hungary", 
     departureDate:"11-13-2015",
+    time:"1200",
     fair:"1587USD"
   },
   {
     origin:"San Francisco, USA",
     destination:"Budapest, Hungary", 
     departureDate:"11-14-2015",
+    time:"1200",
     fair:"1587USD"
   },
   {
     origin:"San Francisco, USA",
     destination:"Istanbul, Turkey", 
     departureDate:"11-2015-2015",
+    time:"1200",
     fair:"1927USD"
   },
   {
     origin:"San Francisco, USA",
     destination:"Istanbul, Turkey", 
     departureDate:"11-16-2015",
+    time:"1200",
     fair:"1927USD"
   },
   {
     origin:"San Francisco, USA",
     destination:"Shanghai, China", 
     departureDate:"11-14-2015",
+    time:"1200",
     fair:"1587USD"
   },
   {
     origin:"San Francisco, USA",
     destination:"Budapest, Hungary", 
     departureDate:"11-12-2015",
+    time:"1200",
     fair:"1587USD"
   },
   {
     origin:"San Francisco, USA",
     destination:"Casablanca, Morroco", 
     departureDate:"11-13-2015",
+    time:"1200",
     fair:"1765USD"
   },
   {
     origin:"San Francisco, USA",
     destination:"Casablanca, Morroco", 
     departureDate:"11-14-2015",
+    time:"1200",
     fair:"1765USD"
   },
   {
     origin:"San Francisco, USA",
     destination:"Casablanca, Morroco", 
     departureDate:"11-15-2015",
+    time:"1200",
     fair:"1765USD"
   },
   {
     origin:"San Francisco, USA",
     destination:"Beirut, Lebanon", 
     departureDate:"11-12-2015",
+    time:"1200",
     fair:"1765USD"
   },
   {
     origin:"San Francisco, USA",
     destination:"Beirut, Lebanon", 
     departureDate:"11-13-2015",
+    time:"1200",
     fair:"1765USD"
   },
   {
     origin:"San Francisco, USA",
     destination:"Beirut, Lebanon", 
     departureDate:"11-15-2015",
+    time:"1200",
     fair:"1765USD"
   },
 ];
