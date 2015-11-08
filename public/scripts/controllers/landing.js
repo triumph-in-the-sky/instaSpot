0c055a6ddf27f233d4db79844987c2c143738a25
shreeya <shreeya.goel@gmail.com>
2015-11-08 00:01:17 -0800

    made wishlist visible on landing page

--- public/scripts/controllers/landing.js
+++ public/scripts/controllers/landing.js
@@ -2,11 +2,13 @@ angular.module('instaSpotApp')
   .controller('LandingCtrl', function($scope, $location, $window){
     $scope.init = function(){
       $('body')[0].style['background-color'] = '#d81921';
-
+      $scope.wish();
       if ($window.localStorage['InstaSpot']) {
-        $scope.wish(JSON.parse($window.localStorage['InstaSpot']));
+        console.log('wishlist exists');
+        $scope.wishlist = true;
+        populateDestinations();
       } else {
-        $scope.empty();
+        $scope.wishlist = false;
       }
     };
 
@@ -15,14 +17,31 @@ angular.module('instaSpotApp')
       $('body')[0].style['background-color'] = '#3b4b54';
     };
 
-    $scope.empty = function(){
-      $('#button_container')[0].style['text-align'] = 'center';
-      $('#button_container')[0].style['margin-top'] = '200px';
-    };
+    // $scope.empty = function(){
+    //   $('#button_container')[0].style['text-align'] = 'center';
+    //   $('#button_container')[0].style['margin-top'] = '200px';
+    // };
 
     $scope.wish = function(){
       $('#button_container')[0].style['position'] = 'fixed';
       $('#button_container')[0].style['bottom'] = '3%';
       $('#button_container')[0].style['right'] = '3%';
     };
+
+    function populateDestinations() {
+      $scope.destinations = [];
+      // console.log($window.localStorage['InstaSpot']);
+      var places = JSON.parse($window.localStorage['InstaSpot']).data;
+      for (var i = 0; i < places.length; i++) {
+        var place = places[i];
+        var city = place[0];
+        var country = place[1];
+        var destination = {
+          city: city, 
+          country: country
+        }
+        $scope.destinations.push(destination);
+      }
+      console.log('wishlist destinations are ', $scope.destinations);
+    }
   })
