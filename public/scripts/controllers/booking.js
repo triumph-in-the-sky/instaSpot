42297ca27e1a6a04fa2a3950f339215150ecdbca
ccheung0926 <cindycheung0926@gmail.com>
2015-11-08 00:03:48 -0800

    [feat] setup get request for flight ticket

--- public/scripts/controllers/booking.js
+++ public/scripts/controllers/booking.js
@@ -1,7 +1,8 @@
 angular.module('instaSpotApp')
-  .controller('BookingCtrl', ['$scope', 'MainFactory', '$window', function ($scope, MainFactory, $window) {
+  .controller('BookingCtrl', ['$scope', 'MainFactory', '$window', '$http', function ($scope, MainFactory, $window, $http) {
     $scope.location = MainFactory.getCity();
-
+    $scope.date = new Date();
+    $scope.flight;
     $scope.save = function(){
       var wishList = $window.localStorage['InstaSpot'];
       var data = MainFactory.getCity();
@@ -16,4 +17,18 @@ angular.module('instaSpotApp')
         $window.localStorage['InstaSpot'] = JSON.stringify(wishList);
       }
     };
+    (function (){
+      $http({
+        method: 'GET',
+        url: '/api/flights'
+      }).then(function successCallback(response) {
+          // this callback will be called asynchronously
+          // when the response is available
+          console.log(response, 'response');
+        }, function errorCallback(response) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        });
+    })();
+
   }]);
