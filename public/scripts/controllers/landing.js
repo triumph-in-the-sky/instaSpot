96c0df1c278e9c1f9706684f7a51251b3f2e47b8
OJ Haugen <ohaugen@usc.edu>
2015-11-07 21:53:40 -0800

    [feat] add conditional placement of button on landing page

--- public/scripts/controllers/landing.js
+++ public/scripts/controllers/landing.js
@@ -1,10 +1,28 @@
 angular.module('instaSpotApp')
-  .controller('LandingCtrl', function($scope, $location){
+  .controller('LandingCtrl', function($scope, $location, $window){
     $scope.init = function(){
       $('body')[0].style['background-color'] = '#d81921';
+
+      if ($window.localStorage['InstaSpot']) {
+        $scope.wish(JSON.parse($window.localStorage['InstaSpot']));
+      } else {
+        $scope.empty();
+      }
     };
+
     $scope.start = function(){
       $location.path('/m');
       $('body')[0].style['background-color'] = '#3b4b54';
+    };
+
+    $scope.empty = function(){
+      $('#button_container')[0].style['text-align'] = 'center';
+      $('#button_container')[0].style['margin-top'] = '200px';
+    };
+
+    $scope.wish = function(){
+      $('#button_container')[0].style['position'] = 'fixed';
+      $('#button_container')[0].style['bottom'] = '3%';
+      $('#button_container')[0].style['right'] = '3%';
     };
   })
