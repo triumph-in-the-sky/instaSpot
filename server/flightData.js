7623d8f0ee329b28970191f273194eec5a044f18
codeCommode <james.harlan@gmail.com>
2015-11-07 18:43:51 -0800

    [feat] added /api/fights and dummy data

---
+++ server/flightData.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "FlightAvailabilityList": [
+    {
+      "FlightNo": "EK542",
+      "FlightDateTime": "23:12",
+      "FlightDate":"",
+      "Sector": "Commercial Asia",
+      "TransitsStations": {
+        "TransitsStation": [
+          "Doha"
+        ]
+      },
+      "Duration": "12h",
+      "FlightFare": "1000AED",
+      "Currency": "AED"
+    },
+    {
+      "FlightNo": "EK545",
+      "FlightDateTime": "09:12",
+      "FlightDate":"",
+      "Sector": "Commercial Asia",
+      "TransitsStations": {
+        "TransitsStation": [
+          ""
+        ]
+      },
+      "Duration": "12h",
+      "FlightFare": "1200AED",
+      "Currency": "AED"
+    }
+  ]
+};
--- server/requestHandlers.js
+++ server/requestHandlers.js
@@ -1,7 +1,13 @@
 var imageList = require('./images');
+var apiUtils = require('./apiUtils');
 
 module.exports = {
   getAllImages: function(req, res){
     res.send(imageList); 
+  },
+  getAllDestinations: function(req, res){
+    apiUtils.getDestinations(function(data){
+      res.send(data);
+    })
   }
 };
